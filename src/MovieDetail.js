91b14d86fc9012936aebbb0e64fea40002c950c4
Peter Ayello-Wright <peter.ayellowright@gmail.com>
2019-03-10 18:36:40 +0000

    Movie db project.

---
+++ src/MovieDetail.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
+
+import { Poster } from './Movie';
+
+const POSTER_PATH = 'https://image.tmdb.org/t/p/w154';
+const BACKDROP_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+class MoviesList extends Component {
+  state = {
+    movie: {},
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=b63b452727b983876f9e6a3301023de8&language=en-US`,
+      );
+
+      const movie = await res.json();
+      this.setState({
+        movie,
+      });
+    } catch (e) {
+      // eslint-disable-line
+      console.log(e);
+    }
+  }
+
+  render() {
+    const { movie } = this.state;
+
+    return (
+      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+        <MovieInfo>
+          <Overdrive id={movie.id}>
+          <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+          </Overdrive>
+          <div>
+            {/* {this.state.movie.title ? (
+              <h1>{movie.title}</h1>
+              <h3>{movie.release_date}</h3>
+              <p>{movie.overview}</p>
+            ) : (
+              <h1>Hi </h1>
+            )} */}
+            <h1>{movie.title}</h1>
+            <h3>{movie.release_date}</h3>
+            <p>{movie.overview}</p>
+          </div>
+        </MovieInfo>
+      </MovieWrapper>
+    );
+  }
+}
+
+export default MoviesList;
+
+const MovieWrapper = styled.div`
+position: relative;
+padding-top: 50vh;
+background: url(${props => props.backdrop}) no-repeat;
+background-size: cover;
+`;
+
+const MovieInfo = styled.div`
+  background: white;
+  text-align: left;
+  padding: 2rem 10%;
+  display: flex;
+  > div {
+    margin-left: 20px;
+  }
+  img {
+    position: relative;
+    top: -5rem;
+  }
+`;
